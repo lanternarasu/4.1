5bc66ba82ae0c68a3c04245955ef253179ce65e3
Admin <Admin@DESKTOP-RTHB91U>
2022-10-27 23:44:12 +0530

    Add project files.

---
+++ 4.1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1
+{
+    internal class Class1
+    {
+        private string[] students = new string[5];
+        private int[] students_mark = new int[5];
+        private char[] g = new char[5];
+        private int[] c = new int[5];
+        public delegate void call_all();
+        public void get_names()
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine("Enter student " + i);
+                students[i] = Console.ReadLine();
+                Console.WriteLine("Enter student mark : ");
+                students_mark[i] = Convert.ToInt32(Console.ReadLine());
+                g[i] = students[i][0];
+                c[i] = (int)g[i];
+            }
+        }
+        public void action()
+        {
+            int n = students.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (c[j] > c[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int itemp = c[j];
+                        c[j] = c[j + 1];
+                        c[j + 1] = itemp;
+                        string temp = students[j];
+                        students[j] = students[j + 1];
+                        students[j + 1] = temp;
+                        int mtemp = students_mark[j];
+                        students_mark[j] = students_mark[j + 1];
+                        students_mark[j + 1] = mtemp;
+                    }
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+                Console.WriteLine(students_mark[i]);
+            }
+        }
+        public void find_highest_mark()
+        {
+            int maxvalue = students_mark.Max();
+            int maxindex = students_mark.ToList().IndexOf(maxvalue);
+            Console.WriteLine("highest mark " + students[maxindex]);
+        }
+    }
+}
---
+++ 4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static _4._1.Class1;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            call_all del_obj_og;
+            call_all del_obj1 = new call_all(obj1.get_names);
+            call_all del_obj2 = new call_all(obj1.action);
+            call_all del_obj3 = new call_all(obj1.find_highest_mark);
+            del_obj_og = del_obj1;
+            del_obj_og += del_obj2;
+            del_obj_og += del_obj3;
+            del_obj_og();
+        }
+    }
+}
